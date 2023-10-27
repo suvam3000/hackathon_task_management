1066f640e36cbc275eca2c678c2bfc33146fb9c3
Suvam Chowdhury <suvchowdhury@microsoft.com>
2023-10-27 11:58:42 +0530

    basic setups

---
+++ intelligent_task_allocation/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class IntelligentTaskAllocationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'intelligent_task_allocation'
---
+++ predictive_task_prioritisation/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PredictiveTaskPrioritisationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'predictive_task_prioritisation'
